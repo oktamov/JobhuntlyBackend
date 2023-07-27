47e8087f7dc910babec06dfc1fe5b7e87c7617de
oktamov <122670933+oktamov@users.noreply.github.com>
2023-07-27 14:46:30 +0500

    vacancy app

--- vacancy/serializers.py
+++ vacancy/serializers.py
@@ -12,3 +12,15 @@ class VacancySerializer(serializers.ModelSerializer):
         fields = (
             'id', 'title', 'experience', 'level', 'job_type', 'salary', 'overview', 'description', 'offer',
             'created_at', 'company', 'user')
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        if user.companies is None:
+            raise serializers.ValidationError('User must belong to a company to create a vacancy.')
+
+        company = validated_data.get('company')
+        if company != user.companies:
+            raise serializers.ValidationError('You can only create vacancies for your own company.')
+
+        validated_data['user'] = user
+        return super().create(validated_data)
--- vacancy/views.py
+++ vacancy/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from vacancy.custom_permission import IsOwnerOrReadOnly
 from vacancy.models import Vacancy
@@ -15,3 +15,16 @@ class VacancyDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = VacancySerializer
     lookup_field = "pk"
     permission_classes = [IsOwnerOrReadOnly]
+
+
+class VacancyCreateView(generics.CreateAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can create a vacancy
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
+
