6de5656a3fb379a8113aebf8e30342d25be46b2c
oktamov <122670933+oktamov@users.noreply.github.com>
2023-07-27 22:56:19 +0500

    vacancy app

--- employee/models.py
+++ employee/models.py
@@ -15,46 +15,7 @@ class Employee(models.Model):
     gender = models.CharField(max_length=50, choices=Gender.choices, default=Gender.MAN)
 
     def __str__(self):
-        return self.user
-
-
-class Experience(models.Model):
-    class Type(models.TextChoices):
-        Part = "part_time"
-        Full = "full_time"
-        Hybrid = "hybrid"
-
-    class Location(models.TextChoices):
-        Remote = "remote"
-        On_site = "on site"
-        Hybrid = "hybrid"
-
-    title = models.CharField(max_length=255)
-    employee = models.ForeignKey(Employee, related_name="experiences", on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, related_name="experiences", on_delete=models.CASCADE)
-    start_year = models.PositiveIntegerField()
-    end_year = models.PositiveIntegerField()
-    work_type = models.CharField(max_length=100, choices=Type.choices, default=Type.Full)
-    location = models.CharField(max_length=100, choices=Location.choices, default=Location.On_site)
-    description = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
-
-
-class Skill(models.Model):
-    name = models.CharField(max_length=255)
-
-
-class EmployeeSkill(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="EmployeeSkills")
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name="EmployeeSkills")
-
-    class Meta:
-        unique_together = ('employee', 'skill')
-
-    def __str__(self):
-        return f"{self.employee} - {self.skill}"
+        return self.username.get_full_name()
 
 
 class Education(models.Model):
